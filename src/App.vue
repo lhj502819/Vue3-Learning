1b21e8a2387b1da7a204f1929f765f547cad0a70
lhj502819 <54894887+lhj502819@users.noreply.github.com>
2024-05-05 22:40:15 +0800

    ref代码示例提交

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="app">
     <h1>你好啊！</h1>
-    <Person/>
+    <Person ref="ren" />
+    <button @click="showRen">展示人标签元素</button>
   </div>
 </template>
 
-<script lang="ts">
-import Person from "@/components/Person.vue";
-export default {
-  name: "App",
-  components: {Person}
-};
+<script lang="ts" setup>
+import Person from "./components/Person.vue";
+import {ref} from 'vue'
+let ren = ref()
+
+function showRen(){
+  console.log(ren.value);
+  
+}
+
 </script>
 
 <style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,41 +1,23 @@
 <template>
-  <div>
-    <h1>需求：监听水温和水位，当水温达到80或者水位达到30，向服务器发请求</h1>
-    <h1>水温：{{ temp }}</h1>
-    <h1>水位：{{ height }}</h1>
-    <button @click="changeTemp">水温+10</button>
-    <button @click="changeHeight">水位+10</button>
+  <div class="person">
+    <h1>中国</h1>
+    <h1 ref="city">北京</h1>
+    <button @click="showCityDom">展示城市的元素</button>
   </div>
 </template>
 
 <script setup lang="ts" name="Person">
-import {ref, reactive, watch, watchEffect} from "vue";
+import {ref, defineExpose} from 'vue'
 
-let temp = ref(20);
-let height = ref(0);
+let city = ref();
+let a = ref(0);
+let b = ref(1);
 
-function changeTemp() {
-  temp.value += 10
+function showCityDom() {
+  console.log(city.value);
 }
 
-function changeHeight() {
-  height.value += 10
-}
-
-//使用watch机制实现，但是需要显示的指明监视的数据
-// watch([temp, height], (newValues, oldValues) => {
-//   if (newValues[0] >= 80 || newValues[1] >= 30) {
-//     console.log('向服务器发请求')
-//   }
-// })
-
-//使用watchEffect，进入页面就会执行一次
-watchEffect(() => {
-  if (temp.value >= 80 || height.value >= 30) {
-    console.log('向服务器发请求')
-  }
-})
-
+defineExpose({city, a, b})
 </script>
 
 <style scoped>
