a415905561861e684a7c908546a0c9339f6b711c
lhj502819 <54894887+lhj502819@users.noreply.github.com>
2024-05-05 14:23:02 +0800

    监视reactive类型的对象数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -6,17 +6,26 @@
     <button @click="inscSum">计数+1</button>
 
     <hr/>
-    <h1>监视情况2：监视ref定义的对象类型数据</h1>
+
+<!--    <h1>监视情况2：监视ref定义的对象类型数据</h1>-->
+<!--    <h2>姓名：{{ person.name }}</h2>-->
+<!--    <h2>姓名：{{ person.age }}</h2>-->
+<!--    <button @click="changeName">修改姓名</button>-->
+<!--    <button @click="changeAge">修改年龄</button>-->
+<!--    <button @click="changePerson">修改人</button>-->
+<!--    <hr/>-->
+    <h1>监视情况3：监视reactive定义的对象类型数据</h1>
     <h2>姓名：{{ person.name }}</h2>
     <h2>姓名：{{ person.age }}</h2>
     <button @click="changeName">修改姓名</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="changePerson">修改人</button>
+
   </div>
 </template>
 
 <script setup lang="ts" name="Person">
-import {ref, watch} from "vue";
+import {ref,reactive, watch} from "vue";
 
 let sum = ref(10);
 
@@ -35,36 +44,32 @@ const stopWatch = watch(sum, (newVal, oldVal) => {
 
 /****************************************************************/
 
-let person = ref({'name': '张三', 'age': 18})
+let person = reactive({'name': '张三', 'age': 18})
 
 function changeName() {
-  person.value.name += '!'
+  person.name += '!'
 }
 
 function changeAge() {
-  person.value.age += 1
+  person.age += 1
 }
 
 function changePerson() {
-  person.value = {'name': '李四', 'age': 29}
+  person = Object.assign(person,{'name': '李四', 'age': 29})
 }
 
-// 若修改的是`ref`定义的对象中的属性，`newValue` 和 `oldValue` 都是新值，因为它们是同一个对象。
-// 若修改整个`ref`定义的对象，`newValue` 是新值， `oldValue` 是旧值，因为不是同一个对象了。
+//监视reactive类型的数据，会自动开启深度监测，并且无法关闭
 watch(person, (newVal, oldValue) => {
   console.log('Person变化了1', newVal, oldValue)
-}, {
-  deep: true,
-  immediate: true
 })
 
 //如果只接一个参数，那value则是新值
-watch(person, value => {
-  console.log('Person变化了2', value)
-}, {
-  deep: true,
-  immediate: true
-})
+// watch(person, value => {
+//   console.log('Person变化了2', value)
+// }, {
+//   deep: true,
+//   immediate: true
+// })
 
 </script>
 
