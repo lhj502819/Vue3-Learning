050c157ff45e2468597120f718d586aa379a7c4d
lihongjian <lihongjian01@51talk.com>
2024-05-08 13:12:17 +0800

    暂时提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div class="app">
-    <h1>你好啊！</h1>
-    //通过v-if控制是否显示Person组件
-    //与v-show的区别：v-show是通过display来控制显示隐藏，v-if是通过添加删除DOM元素来控制显示隐藏，v-if会挂载和卸载组件
-    <Person v-if="showPerson"/>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Person from "@/components/Person.vue";
-import {onBeforeMount, onMounted, ref} from "vue";
 
-let showPerson = ref(true);
-
-onBeforeMount(() => {
-  console.log("父挂载之前")
-})
-
-onMounted(() => {
-  console.log("父挂载完成了")
-})
 </script>
 
 <style>
-.app {
-  background-color: #ddd;
-  box-shadow: 0 0 10px;
-  border-radius: 10px;
-  padding: 20px;
-}
+
 </style>
---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+//创建一个路由器，并暴露出去
+
+//引入createRouter
+import {createRouter, createWebHistory} from "vue-router";
+import Home from "@/components/Home.vue";
+import News from "@/components/News.vue";
+import About from "@/components/About.vue";
+
+//创建路由器，vue3在制定路由的时候需要考虑清楚路由的工作模式
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/news',
+            component: News
+        },
+        {
+            path: '/about',
+            component: About
+        }
+    ]
+});
+
+export default router
