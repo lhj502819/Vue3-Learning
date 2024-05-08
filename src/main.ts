9f49886475285ec8c0e7e8e78465262a9d7d5890
lhj502819 <54894887+lhj502819@users.noreply.github.com>
2024-05-08 22:54:09 +0800

    路由相关代码示例提交

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
+// 引入createAp，用于创建应用
+import {createApp} from 'vue'
 //引入APP根组件
 import App from './App.vue'
-
-createApp(App).mount('#app')
+//引入路由器
+import router from './router/index'
+const app = createApp(App)
+//使用路由器
+app.use(router)
+//挂载整个应用到app容器中
+app.mount('#app')
